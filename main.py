275f6acaa574745d82423a8d9d10ba9dd7bdae0f
Sam Zagrobelny <strangesast@gmail.com>
2015-09-17 17:12:15 -0400

    added secant, modified default threshold

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 # Part III: Bisection method
 
-def bisection(f, a, b, thr=0.01, iters=100):
+def bisection(f, a, b, thr=0.001, iters=100):
     """ Calculates root of function 'f' on interval [a, b] to precision 'thr' in
     at most 'iters' iterations
     """
@@ -37,7 +37,7 @@ if not walk[-1] == None:
     print(walk[-1])
 
 
-def newtons(f, fprime, x0, thr=0.01, iters=100):
+def newtons(f, fprime, x0, thr=0.001, iters=100):
 
     root = None
 
@@ -64,4 +64,35 @@ g = newtons(lambda x: (x-2)**2 - 1, lambda x: 2*(x-2), 0.25)
 
 
 walk = [x for x in g]
-print(walk)
+if not walk[-1] == None:
+    print(walk[-1])
+
+
+def secant(f, x0, x1, thr=0.001, iters=100, sm=0.0001):
+
+    root = None
+
+    for each in range(iters):
+
+        if abs(f(x1) - f(x0)) < sm:
+            # too small
+            break
+
+        x2 = x1 - f(x1)*(x1 - x0) / (f(x1) - f(x0))
+
+        if abs(f(x2)) < thr:
+            root = x2
+            break
+
+        x0 = x1
+        x1 = x2
+
+        yield x2
+
+    yield root
+
+g = secant(lambda x: (x-2)**2 - 1, 0, 2)
+
+walk = [x for x in g]
+
+print(walk[-1])
