60320f3e5d7e9651f80e49e47fcb70ed6770a86f
Sam Zagrobelny <strangesast@gmail.com>
2015-11-16 13:17:52 -0500

    reorganized, plotting energy power and wind

--- project4/plotting.py
+++ project4/plotting.py
@@ -1,12 +1,13 @@
 from __future__ import division
 import os
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import integrate, interpolate
 from matplotlib.dates import YearLocator, MonthLocator, DayLocator, DateFormatter
 import matplotlib.gridspec as gridspec
+from operator import itemgetter
 import datetime
 
 from methods import parse_data_file, interpolate_wind_power_table
@@ -15,8 +16,8 @@ from methods import parse_data_file, interpolate_wind_power_table
 cpath = os.path.dirname(os.path.realpath(__file__))
 data_fname = 'wind_data.csv'
 data_fpath = os.path.join(cpath, data_fname)
-header = ['time', 'temperature', 'wind speed', 'gust', 'direction']
-data = parse_data_file(data_fpath)
+header = ['time', 'temperature', 'wind speed', 'gust', 'delta']
+data, mindate = parse_data_file(data_fpath)
 
 
 def part_one(data):
@@ -95,7 +96,7 @@ def part_one(data):
     afig.savefig(aout_fpath, bbox_inches='tight')
     print('done')
 
-deltas = [each[4] for each in data]
+deltas = [each[header.index('delta')] for each in data]
 dates = [each[header.index('time')] for each in data]
 temps = [each[header.index('temperature')] for each in data]
 winds = [each[header.index('wind speed')] for each in data]
@@ -103,53 +104,69 @@ winds = [each[header.index('wind speed')] for each in data]
 xnew = np.arange(0, 50, 0.1)
 xnew, ynew = interpolate_wind_power_table('power_wind_table.csv', xnew)
 
-interpolated_dict = dict(zip(xnew, ynew))
-interpolated_dict[None] = None
+# convert wind to power in watts (based on interpolated power table)
+interpolated_dict = dict(zip(xnew, ynew)) # conversion dict
+interpolated_dict[None] = None # add None for gaps in data
+powers = [interpolated_dict[wind] for wind in winds] # convert winds to powers
 
-powers = []
-for wind in winds:
-    powers.append(interpolated_dict[wind])
-
-# use 'not None' points to build spline function
-#clean = [(a, b) for a, b in zip(powers, deltas) if a is not None and b is not None]
-#powers, deltas = zip(*clean)
-
-fig = plt.figure(figsize=(16, 4))
-ax = fig.add_subplot(111)
-ax.plot(deltas, powers, 'bo')
-#ax.set_ylim(0)
-ax.set_ylabel('Power (Watts)')
-#fig.savefig('/home/samuel/Downloads/spline.png', bbox_inches='tight')
-
-
-# x, y data
+# x, y data for interpolation (without None's)
 cpowers, cdeltas = zip(*[(a, b) for a, b in zip(powers, deltas) if a is not None and b is not None])
 
-# interpolate
-tck = interpolate.splrep(cdeltas, cpowers, s=0, k=2)
-nones = []
+# interpolation
+tck = interpolate.splrep(cdeltas, cpowers, s=0, k=2) # generate interpolation function
+nones = [] # deltas where power 'is None'
 for xi, yi in zip(deltas, powers):
     if yi is None:
         nones.append(xi)
 
-print(nones)
-mpowers = interpolate.splev(nones, tck, der=0)
-ax.plot(nones, mpowers, 'ro')
+# generate interpolated points from interpolation function
+mpowers = interpolate.splev(nones, tck, der=0) # 'missing powers'
+# NOTE: interpolation function generates low (often negative) values that do not
+# represent intermediate power values
+
+# recombine interpolated and original data
+timedelta_power = zip(cdeltas, cpowers) + zip(nones, mpowers)
+both = sorted(timedelta_power, key=itemgetter(0))
+# recreate time objects from deltas (in seconds) and mindate
+both = map(lambda x: (mindate + datetime.timedelta(seconds=x[0]), x[1]), both)
 
 
-# ident Nones, fill in
+fig = plt.figure(figsize=(16, 6))
+gs = gridspec.GridSpec(3, 10)
+ax = fig.add_subplot(gs[0,:])
+ax2 = fig.add_subplot(gs[1,:])
+ax3 = fig.add_subplot(gs[2,:])
+ax.set_ylabel('Energy (sum)')
+ax2.set_ylabel('Power')
+ax3.set_ylabel('Wind Speed')
 
-xnew = np.arange(0, max(deltas), 600)
-ynew = interpolate.splev(xnew, tck, der=0)
-ax.plot(xnew, ynew, 'g')
+x, y = zip(*both)
+y = [yi if yi > 0 else 0 for yi in y]
+#ax.plot(x, y)
 
-#fig = plt.figure(figsize=(16, 4))
-#powers, deltas = zip(*clean)
+dt, powers = zip(*timedelta_power)
 
-#date_diff = max(dates)-min(dates)
-#
-##tck = interpolate.splrep(powers, deltas, s=0, k=2)
-#xnew = np.arange(0, max(deltas))
-#ynew = interpolate.splev(xnew, tck, der=0)
-#
-#total_power = integrate.simps(ynew, xnew)
+energy = []
+last_tot = 0
+for i, t in enumerate(dt):
+    if i == 0: continue
+    xs = dt[:i]
+    ys = powers[:i]
+
+    total_power = integrate.simps(ys, xs)
+    if total_power < last_tot:
+        total_power = last_tot
+    last_tot = total_power
+    energy.append(total_power)
+
+ax.plot(x[:-20], energy[:-19], linewidth=0.5)
+#ax2 = ax.twinx()
+#ax2.plot(x[:-20], y[:-20], 'r', linewidth=0.5)
+#ax2.set_ylim(0)
+ax2.plot(x[:-20], y[:-20], linewidth=0.5)
+ax3.plot(x[:-20], winds[:-20], linewidth=0.5)
+
+#for a2 in ax2.get_yticklabels():
+#    a2.set_color('r')
+
+fig.savefig('/home/samuel/Downloads/energy_and_power_and_wind.png', bbox_inches='tight', dpi=500)
