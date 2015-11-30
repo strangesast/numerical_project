0660f4ab70f7adeb61b456b9b8f68a3b27b17525
Sam Zagrobelny <strangesast@gmail.com>
2015-11-30 16:42:41 -0500

    cool looking but totally wrong

--- project5/project.py
+++ project5/project.py
@@ -1,7 +1,74 @@
 # MA377 Project 5
+import math
 
-# firing projectile on hill given by
-# hill_height (x) = 
+"""
+g = 9.81 m / s^2 # gravity
+
+v(t) # velocity of projectile at time t
+
+a = -4.75e-8
+b = 2.0e-4
+rho(y) = a y + b # air pressure
+
+air_friction = rho(y) abs( v(t) )^2
+
+x'' = -rho(y) (x')^2
+y'' = -rho(y) (y')^2 - g
+
+
+h(x) = 1000 exp( 10e-6 (x - 4800)^2 ) # hill height over x
+
+x_o = 0 # initial position
+x_target = 5100 # target position
+
+v_o = 350 m/s # initial muzzle velocity
+
+1/2 m v_1^2 = 1/2 m v_0^2 + delta_kinetic + delta_potential 
+
+delta_potential = m g delta_h
+delta_kinetic = -drag_force
+
+"""
+
+# air pressure
+def rho(y):
+    a = -4.75e-8
+    b = 2.0e-4
+    return a*y + b
+
+
+
+
+cos = math.cos
+sin = math.sin
+
+g = 9.81  # m / s^2
+v_0 = 350 # m / s
+
+x_0, y_0 = (0, 0) # m 
+a = math.pi / 4   # rad
+
+v_c = v_0
+x_c = x_0
+y_c = y_0
+
+v_y = v_c*cos(a)
+v_x = v_c*sin(a)
+
+while y_c >= 0:
+    #print('x_c: {}'.format(x_c))
+    print('y_c: {}'.format(y_c))
+
+    x_c += v_x
+    y_c += v_y
+
+    rho_c = rho(y_c) # current air pressure
+
+    d_v_x = -rho_c*abs(v_x)**2
+    d_v_y = -rho_c*abs(v_y)**2 - g
+
+    v_x += d_v_x
+    v_y += d_v_y
 
 # air friction equal to
 # rho (height) abs(velocity (time)) ^ 2
--- project5/web/index.js
+++ project5/web/index.js
@@ -1,5 +1,7 @@
 var canvas_properties = document.getElementById("canvas-properties");
 var target_properties = document.getElementById("target-properties");
+var init_properties = document.getElementById('init-properties')
+var fire_button = document.getElementById('fire');
 
 // get canvas context & reset width + height
 var initialize = function(canvas, width, height) {
@@ -90,6 +92,7 @@ var draw_widths = function(ctx, width, height, range) {
     ctx.fillText(Math.floor(i*100 / width * (range[1] - range[0]) + range[0]) + 'm', 0, 0)
     ctx.restore()
   }
+  ctx.restore()
 }
 
 var determine_height_rat = function(func, range, height, ceiling) {
@@ -126,7 +129,6 @@ initialize_button.addEventListener('click', function(e) {
   e.target.textContent = 'Reset';
   var width = canvas_properties.querySelector("[name=width]").value;
   var height = canvas_properties.querySelector("[name=height]").value;
-
   // get hill function... or just use this
   var hill_function = function(x) {
     return 1000*Math.exp(Math.pow(x-4800, 2)/10e6);
@@ -137,6 +139,7 @@ initialize_button.addEventListener('click', function(e) {
     var hrat = determine_height_rat(hill_function, range, height, 10000);
     draw_background(ctx, hill_function, range, width, height, hrat);
     draw_hill(ctx, hill_function, range, width, height, hrat);
+    // NOTE: need to update this: needs to be modifiable
     draw_start(ctx, hill_function, range, width, height, 0, hrat);
     draw_heights(ctx, width, height, hrat);
     draw_widths(ctx, width, height, range);
@@ -150,10 +153,73 @@ initialize_button.addEventListener('click', function(e) {
     }(ctx);
 
     target_properties.addEventListener('click', target_update_listener);
+
+    var initial_velocity = 350; // m / s
+
+    var fire_button_listener = function(e) {
+      var angle = init_properties.querySelector('[name=initial_angle]').value;
+      var gen = fire(ctx, [0, hill_function(0)], angle, initial_velocity, hill_function, pressure_function, width, height, hrat, range);
+      console.log(gen.next().value);
+    }
+
+    fire_button.addEventListener('click', fire_button_listener);
   });
 });
 
-save_canvas = document.getElementById('save-canvas');
+// air pressure
+var rho = function(y) {
+  var a = -4.75e-8;
+  var b = 2.0e-4;
+  return a*y + b;
+};
+
+var fire = function* (ctx, position, angle, velocity, hill_function, pressure_function, width, height, hrat, range) {
+  var g = 9.81; // m / s^2
+  var angle_rad = angle*Math.PI / 180;
+
+  var x_c = position[0];
+  var y_c = position[1];
+
+  var x_v_c = Math.sin(angle_rad)*velocity
+  var y_v_c = Math.cos(angle_rad)*velocity
+
+  var ret = [];
+
+  ctx.beginPath();
+
+  var xi = ((x_c-range[0])/(range[1]-range[0]))*width;
+  var yi = y_c*hrat;
+
+  ctx.moveTo(xi, yi);
+
+  while (y_c >= hill_function(x_c) && y_c < hill_function(range[0]) + 10000 && x_c > range[0] && x_c < range[1]) {
+    x_c += x_v_c;
+    y_c += y_v_c;
+
+    d_v_x = rho(y_c)*Math.pow(Math.abs(x_v_c), 2);
+    d_v_y = rho(y_c)*Math.pow(Math.abs(y_v_c), 2) + g;
+
+    x_v_c += d_v_x;
+    y_v_c -= d_v_y;
+
+    ret = [x_c, y_c, x_v_c, y_v_c];
+
+    xi = ((x_c-range[0])/(range[1]-range[0]))*width;
+    yi = y_c*hrat;
+    ctx.lineTo(xi, yi);
+  }
+  ctx.strokeStyle = 'red';
+  ctx.stroke();
+  ctx.closePath();
+  ctx.fillStyle = "red";
+  ctx.beginPath();
+  ctx.arc(xi, yi, 10, 0, 2*Math.PI);
+  ctx.closePath();
+  ctx.fill();
+  yield ret;
+}
+
+save_canvas_button = document.getElementById('save-canvas');
 
 save_canvas_event = function(e) {
   var canvas = document.getElementById('canvas');
@@ -162,4 +228,4 @@ save_canvas_event = function(e) {
   w.document.write("<img src='"+data+"' alt='from canvas'/>");
 }
 
-save_canvas.addEventListener('click', save_canvas_event);
+save_canvas_button.addEventListener('click', save_canvas_event);
